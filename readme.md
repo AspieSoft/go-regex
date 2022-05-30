20c58297a1b93d85002586aa038ecfa190cb058b
AspieSoft <aspiesoftweb@gmail.com>
2022-05-30 13:04:24 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,63 @@
+# Go Regex
+
+[![donation link](https://img.shields.io/badge/buy%20me%20a%20coffee-square-blue)](https://buymeacoffee.aspiesoft.com)
+
+Simplifies the the go-pcre regex package.
+After calling a regex, the compiled output gets cached to improve performance.
+
+This package uses the [go-pcre](https://github.com/GRbit/go-pcre) package for better performance.
+
+## Installation
+
+```shell script
+
+  sudo apt-get install libpcre3-dev
+  go get github.com/AspieSoft/go-regex
+
+```
+
+## Usage
+
+```go
+
+import (
+  "github.com/AspieSoft/go-regex/"
+)
+
+// pre compile a regex into the cache
+// this method also returns the compiled pcre.Regexp struct
+regex.Compile(`re`)
+
+// run a replace function (most advanced feature)
+regex.RepFunc(myByteArray, `(?flags)re(capture group)`, func(data func(int) []byte) []byte {
+  data(0) // get the string
+  data(1) // get the first capture group
+
+  return []byte("")
+}, true /* optional: if true, will not process a return output */)
+
+// run a simple light replace function
+regex.RepStr(myByteArray, `re`, myReplacementByteArray)
+
+// return a bool if a regex matches a byte array
+regex.Match(myByteArray, `re`)
+
+// split a byte array in a similar way to JavaScript
+regex.Split(myByteArray, `re|(keep this and split like in JavaScript)`)
+
+// a regex string is modified before compiling, to add a few other features
+`use \' in place of ` + "`" + ` to make things easier`
+`(?#This is a comment in regex)`
+
+// an alias of pcre.Regexp
+regex.Regexp
+
+
+// another helpful function
+// this method makes it easier to return results to a regex function
+regex.JoinBytes("string", []byte("byte array"), 10, 'c', data(2))
+
+// the above method can be used in place of
+append(append(append(append([]byte("string"), []byte("byte array")...), []byte(strconv.Itoa(10))...), 'c'), data(2)...)
+
+```
---
+++ regex.go
@@ -0,0 +1,185 @@
+package regex
+
+import (
+	"bytes"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/GRbit/go-pcre"
+)
+
+type Regexp = pcre.Regexp
+
+var regCache map[string]pcre.Regexp = map[string]pcre.Regexp{}
+
+var regReReplaceQuote pcre.Regexp = pcre.MustCompileJIT(`\\[\\']`, pcre.UTF8, pcre.CONFIG_JIT)
+var regReReplaceComment pcre.Regexp = pcre.MustCompileJIT(`\(\?\#.*?\)`, pcre.UTF8, pcre.CONFIG_JIT)
+
+var varType map[string]reflect.Type
+
+func init() {
+	varType = map[string]reflect.Type{}
+
+	varType["array"] = reflect.TypeOf([]interface{}{})
+	varType["map"] = reflect.TypeOf(map[string]interface{}{})
+
+	varType["int"] = reflect.TypeOf(int(0))
+	varType["float64"] = reflect.TypeOf(float64(0))
+	varType["float32"] = reflect.TypeOf(float32(0))
+
+	varType["string"] = reflect.TypeOf("")
+	varType["byteArray"] = reflect.TypeOf([]byte{})
+	varType["byte"] = reflect.TypeOf([]byte{0}[0])
+
+	// int32 returned instead of byte
+	varType["int32"] = reflect.TypeOf(' ')
+}
+
+func JoinBytes(bytes ...interface{}) []byte {
+	res := []byte{}
+	for _, b := range bytes {
+		switch reflect.TypeOf(b) {
+		case varType["byteArray"]:
+			res = append(res, b.([]byte)...)
+		case varType["byte"]:
+			res = append(res, b.(byte))
+		case varType["int32"]:
+			res = append(res, byte(b.(int32)))
+		case varType["string"]:
+			res = append(res, []byte(b.(string))...)
+		case varType["int"]:
+			res = append(res, []byte(strconv.Itoa(b.(int)))...)
+		}
+	}
+	return res
+}
+
+func Compile(re string) Regexp {
+	if strings.Contains(re, `\'`) {
+		r := []byte(re)
+		ind := regReReplaceQuote.FindAllIndex(r, 0)
+
+		for i := len(ind) - 1; i >= 0; i-- {
+			if r[ind[i][1]-1] == '\'' {
+				r[ind[i][0]-1] = 0
+				r[ind[i][1]-1] = '`'
+			}
+		}
+
+		r = bytes.ReplaceAll(r, []byte{0}, []byte(""))
+		re = string(r)
+	}
+
+	if strings.Contains(re, `(?#`) {
+		re = regReReplaceComment.ReplaceAllString(re, ``, 0)
+	}
+
+	if val, ok := regCache[re]; ok {
+		return val
+	} else {
+		// reg := pcre.MustCompileJIT(re, pcre.UTF8, pcre.CONFIG_JIT)
+		reg := pcre.MustCompile(re, pcre.UTF8)
+		regCache[re] = reg
+		return reg
+	}
+}
+
+func RepFunc(str []byte, re string, rep func(func(int) []byte) []byte, blank ...bool) []byte {
+	reg := Compile(re)
+
+	// ind := reg.FindAllIndex(str, pcre.UTF8)
+	ind := reg.FindAllIndex(str, 0)
+
+	res := []byte{}
+	trim := 0
+	for _, pos := range ind {
+		v := str[pos[0]:pos[1]]
+		m := reg.Matcher(v, 0)
+
+		if len(blank) != 0 {
+			r := rep(func(g int) []byte {
+				return m.Group(g)
+			})
+
+			if r == nil {
+				return nil
+			}
+		} else {
+			if trim == 0 {
+				res = append(res, str[:pos[0]]...)
+			} else {
+				res = append(res, str[trim:pos[0]]...)
+			}
+			trim = pos[1]
+
+			r := rep(func(g int) []byte {
+				return m.Group(g)
+			})
+
+			if r == nil {
+				res = append(res, str[trim:]...)
+				return res
+			}
+
+			res = append(res, r...)
+		}
+	}
+
+	if len(blank) != 0 {
+		return nil
+	}
+
+	res = append(res, str[trim:]...)
+
+	return res
+}
+
+func RepStr(str []byte, re string, rep []byte) []byte {
+	reg := Compile(re)
+
+	// return reg.ReplaceAll(str, rep, pcre.UTF8)
+	return reg.ReplaceAll(str, rep, 0)
+}
+
+func Match(str []byte, re string) bool {
+	reg := Compile(re)
+
+	// return reg.Match(str, pcre.UTF8)
+	return reg.Match(str, 0)
+}
+
+func Split(str []byte, re string) [][]byte {
+	reg := Compile(re)
+
+	ind := reg.FindAllIndex(str, 0)
+
+	res := [][]byte{}
+	trim := 0
+	for _, pos := range ind {
+		v := str[pos[0]:pos[1]]
+		m := reg.Matcher(v, 0)
+		_ = m
+
+		if trim == 0 {
+			res = append(res, str[:pos[0]])
+		} else {
+			res = append(res, str[trim:pos[0]])
+		}
+		trim = pos[1]
+
+		for i := 1; i <= m.Groups; i++ {
+			g := m.Group(i)
+			if len(g) != 0 {
+				res = append(res, m.Group(i))
+			}
+		}
+	}
+
+	e := str[trim:]
+	if len(e) != 0 {
+		res = append(res, str[trim:])
+	}
+
+	return res
+}
